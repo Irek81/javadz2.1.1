05cbb8a3a4457096eb048172b08906d1bc670e11
Irek81 <irekisanbirdin@gmail.com>
2021-07-18 21:43:33 +0500

    main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        int amount = 2_500;
+        int bonus = 20;
+
+        int mile = amount / bonus;
+
+        System.out.println(mile);
+    }
 }
